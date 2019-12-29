9ddf42c0c6b2a710604fbe25113dc55002314d96
ElishevaNafha <57313570+ElishevaNafha@users.noreply.github.com>
2019-12-29 11:04:11 +0200

    Add files via upload

---
+++ dotNet5780_project01_5681_5638/Utilities/Tools.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    public class Tools
+    {
+        
+        public static string GenericToString<T>(T obj)
+        {
+            String str="";
+            foreach (var prop in obj.GetType().GetProperties())
+            {
+                str += prop.Name + "    " + prop.GetMethod.ToString()+"\n";
+            }
+            return str;
+        }
+
+    }
+}
